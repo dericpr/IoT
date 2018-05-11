3b4afb3e11a740558a5fa71b8bd92f74157c8f01
Deric Panet-Raymond <dpraymond@satcomdirect.com>
2018-05-10 22:05:12 -0400

    Complete Dashboard app and cleanup debug message

--- dashboard/index.js
+++ dashboard/index.js
@@ -1,31 +1,27 @@
 //server.js
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var path = require('path');
+var express = require('express');  
+var app = express();  
+var server = require('http').createServer(app);  
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function (socket){
    console.log('connection');
    io.sockets.emit('get_devices');
 
-  socket.on('CH01', function (from, msg) {
-    console.log('MSG', from, ' saying Deric is the best', msg);
-  });
-  socket.on('CH02', function (from, msg) {
-    console.log('MSG2', from, 'sayin ', msg);
-  });
-
   socket.on("device_list", function (data) {
     console.log("Received Device List from App " + data);
+    io.sockets.emit('new_device_data', data);
+    console.log("Sent new device data");
   });
 
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '.', 'public/', 'index.html'))
-  res.end()
+app.get('/devices', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html'); 
 });
 
-http.listen(4000, function () {
+server.listen(4000, function () {
   console.log('listening on *:4000');
 });
